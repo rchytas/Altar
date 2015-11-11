11cc846986db39c570ff6f602763596cea08c4d8
rchytas <mcpandit@gmail.com>
2015-11-11 09:26:17 -0800

    Create SingletonDemo.java

---
+++ SingletonDemo.java
@@ -0,0 +1,15 @@
+public class SingletonDemo {
+
+	// the static singleton object
+	private static final SingletonDemo theObject = new SingletonDemo();
+
+	/**
+	 * private constructor
+	 */
+	private SingletonDemo() {
+	}
+
+	public SingletonDemo getInstance() {
+		return theObject;
+	}
+}
