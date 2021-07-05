7b392b4f65a045b1ae30e1fe0429580ba84c73d2
rchytas <mcpandit@gmail.com>
2021-07-05 15:34:13 -0700

    Added array based unit test for FizzBuzz

--- com/rchytas/examples/FizzBuzzTest.java
+++ com/rchytas/examples/FizzBuzzTest.java
@@ -1,5 +1,6 @@
 package com.rchytas.examples;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -10,14 +11,20 @@ import org.junit.Test;
  * For numbers which are multiples of both 3 and 5 print “FizzBuzz”.
  * 
  * Also, provide Unit Test for the program.
- *  
+ * 
  * @author mpandit
  *
  */
+
 public class FizzBuzzTest {
 
 	@Test
 	public void testFizzBuzz() {
+		// FizzBuzz Unit Test using array
+		int k = 5;
+		String expected_result[] = { "1", "2", "Fizz", "4", "Buzz" };
+		assertArrayEquals(expected_result, fizzBuzzArr(k));
+		// FizzBuzz Unit Test using individual numbers
 		assertEquals("1", fizzBuzz(1));
 		assertEquals("2", fizzBuzz(2));
 		assertEquals("Fizz", fizzBuzz(3));
@@ -25,12 +32,42 @@ public class FizzBuzzTest {
 		assertEquals("Fizz", fizzBuzz(9));
 		assertEquals("Buzz", fizzBuzz(5));
 		assertEquals("Buzz", fizzBuzz(10));
-		assertEquals("Buzz", fizzBuzz(15));
+		//assertEquals("Buzz", fizzBuzz(15));
 		assertEquals("FizzBuzz", fizzBuzz(15));
 		int n = 100;
 		printMe(n);
 	}
+	
+	/**
+	 * Returns array for FizzBuzz Unit Test example
+	 * 
+	 * @param n
+	 * @return
+	 */
+	public String[] fizzBuzzArr(int n) {
+		String result[] = new String[n];
+		for (int i = 1; i <= n; i++) {
+			String output = "";
+			if (i % 3 == 0) {
+				output += "Fizz";
+			}
+			if (i % 5 == 0) {
+				output += "Buzz";
+			}
+			if (i % 3 != 0 && i % 5 != 0) {
+				output = String.valueOf(i);
+			}
+			result[i - 1] = output;
+		}
+		return result;
+	}
 
+	/**
+	 * FizzBuzz
+	 * 
+	 * @param input
+	 * @return
+	 */
 	private String fizzBuzz(int input) {
 		String output = "";
 		if (input % 3 == 0) {
@@ -45,6 +82,10 @@ public class FizzBuzzTest {
 		return output;
 	}
 
+	/**
+	 * 
+	 * @param n
+	 */
 	private void printMe(int n) {
 		for (int i = 1; i <= n; i++) {
 			System.out.println(fizzBuzz(i));
